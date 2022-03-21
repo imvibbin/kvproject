5958e49da659179cd552f5d08e1ddb6f67a6ed2c
John Kervin Valle Cabuntasan <johnkvcode31@gmail.com>
2022-03-21 17:36:21 +0100

    placed old angular project into the new one

--- kvProject/src/app/app.module.ts
+++ kvProject/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './general-components/navbar/navbar.component';
+import { FooterComponent } from './general-components/footer/footer.component';
+import { HomepageComponent } from './website-apps/homepage-components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
